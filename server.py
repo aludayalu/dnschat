5d3885f9e6cf41747b022abc26465ff79a018b87
Aarav Dayal <pl46u3.ducklinz@gmail.com>
2025-08-11 21:30:01 +0530

    basic DNS server echo server

---
+++ server.py
@@ -0,0 +1,16 @@
+from dnslib.server import DNSServer, BaseResolver
+from dnslib import RR, QTYPE, TXT
+
+class EchoResolver(BaseResolver):
+    def resolve(self, request, handler):
+        qname = request.q.qname
+        labels = str(qname).rstrip('.').split('.')
+        msg = labels[0]
+        reply_msg = f"Echoing {msg}"
+        reply = request.reply()
+        reply.add_answer(RR(qname, QTYPE.TXT, rdata=TXT(reply_msg), ttl=60))
+        return reply
+
+resolver = EchoResolver()
+server = DNSServer(resolver, port=53, address="0.0.0.0")
+server.start()
